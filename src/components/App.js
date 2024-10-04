7d88944715d7e2b7fd5060ffcb3243914ce49f40
João Pedro Martins Forte <joao.forte@fatec.sp.gov.br>
2024-10-04 17:59:04 -0300

    feat(p1-pdmt): cria componente de entrada

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react'
+import LembreteEntrada from './LembreteEntrada'
 
 export default class App extends Component {
+    state = {
+        lembretes: []
+    }
+
+    salvarLembrete = (lembrete) => {
+        this.state.lembretes.push(lembrete)
+        console.log(this.state.lembretes)
+    }
+
     render() {
         return (
             <div className='container'>
                 <div className="row">
-                    <div className="col-sm-12 col-md-6 col-lg-4 bg-primary text-white">
-                        Hello, lembretes
+                    <div className="col-sm-12 col-md-6 col-lg-4">
+                        
                     </div>
                 </div>
+                <LembreteEntrada 
+                    placeholderCampo='Digite seu novo lembrete'
+                    textoBotao='Salvar'
+                    funcao={this.salvarLembrete}
+                />
             </div>
         )
     }
---
+++ src/components/LembreteEntrada.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+export default class LembreteEntrada extends Component {
+    state = {
+        termo: ''
+    }
+
+    onTermoAlterado = (evento) => {
+        this.setState({
+            termo: evento.target.value
+        })
+    }
+
+    onFormSubmit = (evento) => {
+        evento.preventDefault()
+        this.props.funcao(this.state.termo)
+        this.setState({
+            termo: ''
+        })
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onFormSubmit} className='mt-4'>
+                <input 
+                    type="text" 
+                    name="campo-lembrete" 
+                    id="campo-lembrete" 
+                    placeholder={this.props.placeholderCampo} 
+                    value={this.state.termo} 
+                    onChange={this.onTermoAlterado} 
+                    className='form-control mb-3'
+                />
+                <button type='submit' className='btn btn-primary w-100'>
+                    {this.props.textoBotao}
+                </button>
+            </form>
+        )
+    }
+}
+
+LembreteEntrada.defaultProps = {
+    placeholderCampo: 'Buscar...',
+    textoBotao: 'OK'
+}
