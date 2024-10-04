cbe05c31fd714bee3fe7b51ba15f36f2d5f77606
João Pedro Martins Forte <joao.forte@fatec.sp.gov.br>
2024-10-04 16:44:47 -0300

    feat(p1-pdmt): cria componente principal e faz hello

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className='container'>
+                <div className="row">
+                    <div className="col-sm-12 col-md-6 col-lg-4 bg-primary text-white">
+                        Hello, lembretes
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
